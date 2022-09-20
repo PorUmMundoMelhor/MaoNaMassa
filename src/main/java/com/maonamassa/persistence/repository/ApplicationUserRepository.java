984b3e0461090676a36c32a682341f7674385a57
Fabio Munhoz <111292728+PorUmMundoMelhor@users.noreply.github.com>
2022-09-20 17:43:33 -0300

    0.14

---
+++ src/main/java/com/maonamassa/persistence/repository/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.maonamassa.persistence.repository;
+
+import com.maonamassa.persistence.model.ApplicationUser;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ApplicationUserRepository extends PagingAndSortingRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
--- src/main/java/com/maonamassa/security/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/maonamassa/security/filter/JWTAuthenticationFilter.java
@@ -2,8 +2,12 @@ package com.maonamassa.security.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.maonamassa.persistence.model.ApplicationUser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -12,12 +16,16 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.security.Signature;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
-import static com.maonamassa.security.filter.Constants.EXPIRANTION_TIME;
+import static com.maonamassa.security.filter.Constants.*;
+import static java.util.Date.from;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
@@ -30,7 +38,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
             ApplicationUser user = new ObjectMapper().readValue(request.getInputStream(), ApplicationUser.class);
-            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()))
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -40,8 +48,20 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         ZonedDateTime expTimeUTC = ZonedDateTime.now(ZoneOffset.UTC).plus(EXPIRANTION_TIME, ChronoUnit.MILLIS);
         String token = Jwts.builder()
-                .setSubject
+                .setSubject(((ApplicationUser) authResult.getPrincipal()).getUsername())
+                .setExpiration(Date.from(expTimeUTC.toInstant()))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+
+        token = TOKEN_PREFIX + token;
+        String tokenJson = "{\"token\":" + addQuotes(TOKEN_PREFIX + token) + ", \"exp\":" + addQuotes(expTimeUTC.toString()) + "}";
+        response.getWriter().write(tokenJson);
+        response.addHeader("Content-Type", "application/json;charset=UTF-8");
+        response.addHeader(HEADER_STRING, token);
     }
 
+    private String addQuotes(String value) {
+        return new StringBuilder(300).append("\"").append(value).append("\"").toString();
+    }
 
 }
